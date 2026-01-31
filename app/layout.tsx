047e3d88a98766a3135fc5d7fa516fc7b8d673ee
Mahima Subhashana <mahimasubhashana@gmail.com>
2026-01-31 10:54:59 +0530

    add pint

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "@/components/Footer";
 import { WishlistProvider } from "@/app/context/WishlistContext";
+import { Inter } from "next/font/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,12 +16,19 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const inter = Inter({ subsets: ["latin"] });
+
 export const metadata: Metadata = {
   title: {
     default: "Daily Decor Finds",
     template: "%s | Daily Decor Finds", // Show other pages
   },
   description: "Discover curated home decor, cozy lighting, and aesthetic tech accessories for your dream space.",
+  verification: {
+    other: {
+      "p:domain_verify": "20d8b6bf7ab1ac802df374fe3ccdb3eb",
+    },
+  },
 };
 
 export default function RootLayout({
